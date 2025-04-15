614fe32d3f2a928bf2dd688effdf44253ab06ba8
Fazal2307-dev <mdfazal2307@gmail.com>
2025-04-15 10:00:27 +0530

    tailwind

--- tailwindPractices/src/App.jsx
+++ tailwindPractices/src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      
+      <button className='p-1/2 m-4 bg-amber-400 flex flex-wrap justify-content:center'>click on me</button>
     </div>
   )
 }
