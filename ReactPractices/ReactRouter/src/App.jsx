c0e9096afad71f4954571e9428d4f9932e92208d
Fazal2307-dev <mdfazal2307@gmail.com>
2025-04-17 15:11:05 +0530

    practices

--- ReactPractices/ReactRouter/src/App.jsx
+++ ReactPractices/ReactRouter/src/App.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import Navbar from './components/Navbar'
+import {Routes,Route} from "react-router-dom"
+import Home from './Pages/Home'
+import Product from './Pages/Product'
+import Contact from './Pages/Contact'
+import About from './Pages/About'
 
 const App = () => {
   return (
     <div>
-      
+      <Navbar />
+      <div className='container'> 
+              <Routes>
+                <Route path='/' element={<Home />}/>
+                <Route path='/product' element={<Product />}/>
+                <Route path='/contact' element={<Contact />}/>
+                <Route path='/about' element={<About />}/>
+              </Routes>
+      </div>
     </div>
   )
 }
