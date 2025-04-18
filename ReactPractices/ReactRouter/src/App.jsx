a3a816c912acea5afc16e3acfac1825eb98c3ceb
Fazal2307-dev <mdfazal2307@gmail.com>
2025-04-18 11:36:19 +0530

    adding  routing

--- ReactPractices/ReactRouter/src/App.jsx
+++ ReactPractices/ReactRouter/src/App.jsx
@@ -1,24 +1,25 @@
 import React from 'react'
-import Navbar from './components/Navbar'
-import {Routes,Route} from "react-router-dom"
+import {Route ,createBrowserRouter,createRoutesFromElements,RouterProvider} from "react-router-dom"
 import Home from './Pages/Home'
 import Product from './Pages/Product'
 import Contact from './Pages/Contact'
 import About from './Pages/About'
+import RootLayout from './RootLayout/RootLayout'
 
 const App = () => {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path='/' element={<RootLayout/>} >
+                <Route index element={<Home />}/>
+                <Route path='product' element={<Product />}/>
+                <Route path='contact' element={<Contact />}/>
+                <Route path='about' element={<About />}/>
+
+      </Route>
+    )
+  )
   return (
-    <div>
-      <Navbar />
-      <div className='container'> 
-              <Routes>
-                <Route path='/' element={<Home />}/>
-                <Route path='/product' element={<Product />}/>
-                <Route path='/contact' element={<Contact />}/>
-                <Route path='/about' element={<About />}/>
-              </Routes>
-      </div>
-    </div>
+  <RouterProvider  router={router} />
   )
 }
 
