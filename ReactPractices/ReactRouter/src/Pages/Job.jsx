996243104d0503e88b53626d30e6a6b93be03949
Fazal2307-dev <mdfazal2307@gmail.com>
2025-04-19 19:10:15 +0530

    update job page

--- ReactPractices/ReactRouter/src/Pages/Job.jsx
+++ ReactPractices/ReactRouter/src/Pages/Job.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
+import { Link, useLoaderData } from 'react-router-dom'
 
 const Job = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const jobData = useLoaderData();
 
-export default Job
+  return (
+    <div className="job-list">
+      {jobData.map((job) => (
+        <Link key={job.id} to={`/jobs/${job.id}`} className="job-item">
+          <h3>{job.title}</h3>
+          <p>{job.company}</p>
+          <p>{job.location}</p>
+        </Link>
+      ))}
+    </div>
+  );
+};
+
+export default Job;
+
+// eslint-disable-next-line react-refresh/only-export-components
+export  const jobsLoader = async ()=>{
+  const  res=  await fetch("http://localhost:5000/jobs")
+  return res.json();
+ }
